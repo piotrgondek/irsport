f1ea69536a4d255df12521f1f6c587b7c478714e
Piotr Gondek <piotr.gondek@ocado.com>
2021-09-27 10:52:42 +0200

    fix(#35): make e2e tests working with react-youtube

--- src/Movies.tsx
+++ src/Movies.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {
   Box,
+  Card,
+  CardContent,
+  CardMedia,
   Grid,
-  useMediaQuery,
 } from '@mui/material';
+import { PlayCircleFilledWhite } from '@mui/icons-material';
 import YouTube from 'react-youtube';
 import SectionGrid from './SectionGrid';
-import { dark } from './themes';
 
 const Movies = React.forwardRef((_, ref) => {
-  const md = useMediaQuery(dark.breakpoints.up('md'));
+  const [videoId, setVideoId] = React.useState<string | undefined>();
 
   const videos = React.useMemo<string[]>(
     () => [
@@ -23,6 +25,10 @@ const Movies = React.forwardRef((_, ref) => {
     ],
     [],
   );
+
+  const playVideo = (video: string) => () => {
+    setVideoId(video);
+  };
 
   return (
     <Box
@@ -41,13 +47,62 @@ const Movies = React.forwardRef((_, ref) => {
               md={6}
               xs={12}
             >
-              <YouTube
-                videoId={video}
-                opts={{
-                  width: '100%',
-                  height: md ? '200px' : '160px',
-                }}
-              />
+              <Card>
+                {videoId === video
+                  ? (
+                    <CardContent
+                      sx={{
+                        height: {
+                          xs: '160px',
+                          md: '200px',
+                        },
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        p: 0,
+                        '&:last-child': {
+                          p: 0,
+                        },
+                      }}
+                    >
+                      <YouTube
+                        videoId={video}
+                        opts={{
+                          width: '100%',
+                          height: '100%',
+                          playerVars: {
+                            autoplay: 1,
+                            color: 'white',
+                          },
+                        }}
+                      />
+                    </CardContent>
+                  )
+                  : (
+                    <CardMedia
+                      onClick={playVideo(video)}
+                      image={`http://img.youtube.com/vi/${video}/0.jpg`}
+                      sx={{
+                        backgroundPosition: 'center',
+                        backgroundSize: 'auto',
+                        cursor: 'pointer',
+                        height: {
+                          xs: '160px',
+                          md: '200px',
+                        },
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <PlayCircleFilledWhite
+                        sx={{
+                          fontSize: '5rem',
+                        }}
+                      />
+                    </CardMedia>
+                  )}
+              </Card>
             </Grid>
           ))}
         </Grid>
