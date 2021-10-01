715a724062d50e2c93161e2a341bcee773efda6b
Piotr Gondek <piotr.gondek@ocado.com>
2021-10-01 15:57:10 +0200

    fix: make Google Map working

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,58 +1,40 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import {
   Box,
   Typography,
 } from '@mui/material';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleApiWrapper, Map } from 'google-maps-react';
 import SelectionGrid from './SectionGrid';
 
-const Map = React.forwardRef((_, ref) => {
-  const [, setMap] = React.useState(null);
+const MapWrapper = GoogleApiWrapper({
+  apiKey: 'AIzaSyBQBLIXaLT-KEO35rsZ9KShhJEX5-1ZTuo',
+})(({ google }) => (
+  <Map
+    google={google}
+    zoom={21}
+    containerStyle={{
+      position: 'relative',
+      width: '100%',
+      height: '75vh',
+    }}
+    style={{
+      width: '100%',
+      height: '100%',
+    }}
+    initialCenter={{
+      lat: 50.07583557437312,
+      lng: 19.930395333048864,
+    }}
+  />
+));
 
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: 'AIzaSyBQBLIXaLT-KEO35rsZ9KShhJEX5-1ZTuo',
-  });
+const MapContainer = React.forwardRef((_, ref) => (
+  <Box ref={ref as any}>
+    <SelectionGrid>
+      <Typography variant="h2">ul. Wrocławska 11a Kraków</Typography>
+      <MapWrapper />
+    </SelectionGrid>
+  </Box>
+));
 
-  const onLoad = React.useCallback((map) => {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map);
-  }, []);
-
-  const onUnmount = React.useCallback(() => {
-    setMap(null);
-  }, []);
-
-  return (
-    <Box ref={ref as any}>
-      <SelectionGrid>
-        <Typography variant="h2">ul. Wrocławska 11a Kraków</Typography>
-        {isLoaded ? (
-          <Box
-            sx={{
-              height: '70vh',
-            }}
-          >
-            <GoogleMap
-              mapContainerStyle={{
-                width: '100%',
-                height: '100%',
-              }}
-              center={{
-                lat: 50.07583557437312,
-                lng: 19.930395333048864,
-              }}
-              zoom={21}
-              onLoad={onLoad}
-              onUnmount={onUnmount}
-            />
-          </Box>
-        ) : <></>}
-      </SelectionGrid>
-    </Box>
-  );
-});
-
-export default React.memo(Map);
+export default MapContainer;
