5e861e5142dab53e746265842071fd72f5946317
Piotr Gondek <piotr.gondek@ocado.com>
2021-10-06 13:08:00 +0200

    feat(#77): introduce about club section

--- src/Coach.tsx
+++ src/Coach.tsx
@@ -4,10 +4,12 @@ import {
   Card,
   CardContent,
   CardMedia,
+  Grid,
   Typography,
 } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import adamWargal from './assets/AdamWargal.jpg';
+import IRS from './assets/IRS.jpg';
 import SectionGrid from './SectionGrid';
 
 const Coach = React.forwardRef<HTMLElement>((_, ref) => {
@@ -16,27 +18,71 @@ const Coach = React.forwardRef<HTMLElement>((_, ref) => {
   return (
     <Box ref={ref as any}>
       <SectionGrid>
-        <Card>
-          <CardMedia
-            style={{ paddingTop: '56.25%' }}
-            title="Adam Wargal"
-            image={adamWargal}
-          />
-          <CardContent>
-            <Typography variant="h2">{t('coach.name')}</Typography>
-            <Typography variant="body1" gutterBottom>
-              {t('coach.description.p1')}
-            </Typography>
+        <Grid
+          container
+          spacing={4}
+        >
+          <Grid
+            item
+            xs={12}
+            md={6}
+          >
+            <Card>
+              <CardMedia
+                title="Adam Wargal"
+                image={adamWargal}
+                sx={{
+                  paddingTop: '56.25%',
+                }}
+              />
+              <CardContent>
+                <Typography variant="h2">{t('coach.coach')}</Typography>
+                <Typography variant="h4">{t('coach.name')}</Typography>
+                <Typography variant="body1" gutterBottom>
+                  {t('coach.description.p1')}
+                </Typography>
 
-            <Typography variant="body1" gutterBottom>
-              {t('coach.description.p2')}
-            </Typography>
+                <Typography variant="body1" gutterBottom>
+                  {t('coach.description.p2')}
+                </Typography>
 
-            <Typography variant="body1">
-              {t('coach.description.p3')}
-            </Typography>
-          </CardContent>
-        </Card>
+                <Typography variant="body1">
+                  {t('coach.description.p3')}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={6}
+          >
+            <Card>
+              <CardMedia
+                title="IRS"
+                image={IRS}
+                sx={{
+                  paddingTop: '56.25%',
+                }}
+              />
+              <CardContent>
+                <Typography variant="h2">{t('club.club')}</Typography>
+                <Typography variant="h4">{t('club.name')}</Typography>
+                <Typography variant="body1" gutterBottom>
+                  {t('club.description.p1')}
+                </Typography>
+
+                <Typography variant="body1" gutterBottom>
+                  {t('club.description.p2')}
+                </Typography>
+
+                <Typography variant="body1">
+                  {t('club.description.p3')}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
       </SectionGrid>
     </Box>
   );
