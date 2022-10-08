ebffd3d10c56b3b1ff640ccb91213d9e4712dadd
Piotr Marian Jan Gondek <piotrgondek+github@protonmail.com>
2022-10-08 11:25:44 +0200

    feat: new cooperations

--- src/Cooperation.tsx
+++ src/Cooperation.tsx
@@ -3,26 +3,33 @@ import { Grid } from '@mui/material';
 import benefitSystems from './assets/logos/benefit-systems.jpg';
 import medicover from './assets/logos/Mcov-sport-pion.jpg';
 import fit from './assets/logos/fit.jpg';
+import pzkb1 from './assets/logos/logo PZKB.jpg';
+import pzkb2 from './assets/logos/pobraneLogo PZB.jpg';
+
+const images = [
+  {
+    src: pzkb1,
+    name: 'pzkb1',
+  },
+  {
+    src: pzkb2,
+    name: 'pzkb2',
+  },
+  {
+    src: benefitSystems,
+    name: 'Benefit Systems',
+  },
+  {
+    src: medicover,
+    name: 'Medicover',
+  },
+  {
+    src: fit,
+    name: 'FitProfit',
+  },
+];
 
 const Cooperation: React.FC = () => {
-  const images = React.useMemo(
-    () => [
-      {
-        src: benefitSystems,
-        name: 'Benefit Systems',
-      },
-      {
-        src: medicover,
-        name: 'Medicover',
-      },
-      {
-        src: fit,
-        name: 'FitProfit',
-      },
-    ],
-    []
-  );
-
   return (
     <Grid
       container
@@ -35,7 +42,7 @@ const Cooperation: React.FC = () => {
     >
       {images.map((image) => (
         <Grid item key={image.name}>
-          <img src={image.src} alt={image.name} />
+          <img src={image.src} alt={image.name} style={{ maxWidth: '200px' }} />
         </Grid>
       ))}
     </Grid>
