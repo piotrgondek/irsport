8f310d5d99757e714161ba719138fc23819e1fc8
Piotr Marian Jan Gondek <piotrgondek+github@protonmail.com>
2023-08-24 12:44:37 +0200

    feat(timetable): introduce new timetable

--- src/data/trainings.tsx
+++ src/data/trainings.tsx
@@ -1,15 +1,24 @@
 import { CustomTypeOptions } from 'react-i18next';
 
-type Day = 'mo' | 'tu' | 'we' | 'th' | 'fr';
+type Day = 'mo' | 'tu' | 'we' | 'th' | 'fr' | 'sa';
 
-export const days: Array<Day> = ['mo', 'tu', 'we', 'th', 'fr'];
+export const days: Array<Day> = ['mo', 'tu', 'we', 'th', 'fr', 'sa'];
 
 type TimeTableKeys = keyof CustomTypeOptions['resources']['pl']['timetable'];
 
 export type CellContent = {
   body1: TimeTableKeys;
   body2?: TimeTableKeys;
-  overline: '8:00 - 9:00' | '17:00 - 17:45' | '17:00 - 18:00' | '18:00 - 19:00' | '19:00 - 20:00' | '20:00 - 21:00';
+  overline:
+    | '8:00 - 9:00'
+    | '11:00 - 12:30'
+    | '16:00 - 17:00'
+    | '16:15 - 17:00'
+    | '17:00 - 18:00'
+    | '18:00 - 19:00'
+    | '19:00 - 20:00'
+    | '20:00 - 21:00'
+    | '20:00 - 21:30';
 };
 
 type Table = Record<Day, Array<CellContent | null>>;
@@ -17,34 +26,48 @@ type Table = Record<Day, Array<CellContent | null>>;
 export const table: Table = {
   mo: [
     null,
+    null,
+    {
+      body1: 'irsKids',
+      body2: '8to13',
+      overline: '16:00 - 17:00',
+    },
     {
       body1: 'kickboxing',
-      body2: 'irsKids',
-      overline: '17:00 - 17:45',
+      body2: 'beginner',
+      overline: '17:00 - 18:00',
     },
     {
       body1: 'boxingWomen',
+      body2: 'beginner',
       overline: '18:00 - 19:00',
     },
     {
       body1: 'boxing',
       body2: 'advanced',
-      overline: '19:00 - 20:00',
+      overline: '20:00 - 21:00',
     },
   ],
   tu: [
     {
       body1: 'boxing',
-      body2: 'enrolments',
+      body2: 'combined',
       overline: '8:00 - 9:00',
+    },
+    null,
+    {
+      body1: 'irsKids',
+      body2: '4to7',
+      overline: '16:15 - 17:00',
     },
     {
       body1: 'kickboxing',
-      body2: 'combined',
+      body2: 'advanced',
       overline: '17:00 - 18:00',
     },
     {
-      body1: 'shieldInstruments',
+      body1: 'fightCross',
+      body2: 'shieldInstruments',
       overline: '18:00 - 19:00',
     },
     {
@@ -55,35 +78,51 @@ export const table: Table = {
   ],
   we: [
     null,
+    null,
+    {
+      body1: 'irsKids',
+      body2: '8to13',
+      overline: '16:00 - 17:00',
+    },
     {
       body1: 'kickboxing',
-      body2: 'irsKids',
-      overline: '17:00 - 17:45',
+      body2: 'beginner',
+      overline: '17:00 - 18:00',
     },
     {
       body1: 'boxing',
-      body2: 'competition',
+      body2: 'advanced',
       overline: '18:00 - 19:00',
     },
     {
-      body1: 'boxing',
-      body2: 'sparrings',
+      body1: 'boxingSparrings',
       overline: '19:00 - 20:00',
+    },
+    {
+      body1: 'open',
+      overline: '20:00 - 21:30',
     },
   ],
   th: [
     {
       body1: 'boxing',
-      body2: 'enrolments',
+      body2: 'combined',
       overline: '8:00 - 9:00',
+    },
+    null,
+    {
+      body1: 'irsKids',
+      body2: '4to7',
+      overline: '16:15 - 17:00',
     },
     {
       body1: 'kickboxing',
-      body2: 'combined',
+      body2: 'advanced',
       overline: '17:00 - 18:00',
     },
     {
       body1: 'boxingWomen',
+      body2: 'advanced',
       overline: '18:00 - 19:00',
     },
     {
@@ -91,23 +130,33 @@ export const table: Table = {
       body2: 'advanced',
       overline: '19:00 - 20:00',
     },
-    {
-      body1: 'boxing',
-      body2: 'competition',
-      overline: '20:00 - 21:00',
-    },
   ],
   fr: [
     null,
     null,
+    null,
+    null,
     {
       body1: 'boxingWomen',
+      body2: 'beginner',
       overline: '18:00 - 19:00',
     },
     {
       body1: 'boxing',
       body2: 'beginner',
       overline: '19:00 - 20:00',
+    },
+    {
+      body1: 'open',
+      overline: '20:00 - 21:30',
+    },
+  ],
+  sa: [
+    null,
+    {
+      body1: 'boxing',
+      body2: 'combined',
+      overline: '11:00 - 12:30',
     },
   ],
 };
